bc97a42eb23be881bc93aa088a7dc1e1bc7f067c
Viktor Maksimoski <viktor.maksimoski@outlook.com>
2025-10-04 23:20:26 +0200

    final coomit

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,17 +4,21 @@ import { connectDB } from './config/db.js'
 import dotenv from "dotenv"
 import rateLimiter from './middlewares/rateLimiter.js'
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 console.log(process.env.MONGO)
 
 const app = express();
+const __dirname = path.resolve()
 
 //middlewares
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }))
+}
 
 app.use(express.json())
 app.use(rateLimiter)
@@ -22,6 +26,14 @@ app.use(rateLimiter)
 
 //routes
 app.use("/api/notes", notesRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get((req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"))
+    })
+}
 
 connectDB().then(() => {
     app.listen(process.env.PORT, () => {
