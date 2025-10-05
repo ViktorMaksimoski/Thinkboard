b8474ecf988c9998b093530c9803a1360c688937
ViktorMaksimoski <103445246+ViktorMaksimoski@users.noreply.github.com>
2025-10-05 14:23:37 +0200

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const URL = import.meta.env.MODE == "development" ? "http://localhost:5001/api" : "/api"
+
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: URL
 })
 
 export default api;
